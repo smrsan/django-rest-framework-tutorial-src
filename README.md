f0fdb434e5485d06ceed8f7eda0d1c2701b7888b
smrsan <smrsan@users.noreply.github.com>
2020-01-11 13:55:57 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# django-rest-framework-tutorial-src
+My Written Codes for when I was Studying The Rest Framework :)
---
+++ snippets/urls.py
@@ -0,0 +1,60 @@
+from django.urls import path, include
+
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+from .views import SnippetViewSet, UserViewSet, api_root
+
+
+######## 6. ViewSets and Routers (Tutorial Sample) [start] ########
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet)
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
+
+
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
+
+
+# urlpatterns = [
+#     path('', api_root),
+#     path('snippets/', snippet_list, name='snippet-list'),
+#     path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+#     path('snippets/<int:pk>/highlight/',
+#          snippet_highlight, name='snippet-highlight'),
+#     path('users/', user_list, name='user-list'),
+#     path('users/<int:pk>/', user_detail, name='user-detail'),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+######## 4. Authentication and Permissions (Tutorial Sample) ########
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls'))
+]
